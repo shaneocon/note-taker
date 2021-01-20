e047cfc432fe4035a05c50580a4e503025db9e85
shaneocon <shane.pat.oconnell@gmail.com>
2021-01-19 21:23:00 -0800

    delete function

--- js/apiRoute.js
+++ js/apiRoute.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const util = require("util");
-const contents = require("../db/contents");
+var contents = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
@@ -29,25 +29,23 @@ const api = function(app) {
         res.json(newNote);
     });
 
-    app.delete("/api/notes/:id", function(req,res) {
-
-        console.log("Req.params:", req.params);
-        let chosenId = parseInt(req.params.id);
-        console.log(chosenId);
-        
-        for (let i = 0; i < contents.length; i++) {
-            if (chosenId === contents[i].id) {
-                contents.splice(i, 1);
-
-                let noteJSON = JSON.stringify(contents, null, 2);
-
-                writeFileAsync("./db/db.json", noteJSON).then(function() {
-                    console.log("Chosen note deleted.");
-                });
-            }
+    app.delete("/api/notes/:id", function(req, res) {
+        let noteId = req.params.id;
+        let newId = 0;
+        // Console logging deleted note ID for reference
+        console.log(`Deleting note with ID ${noteId}`);
+        // Filter data of current note, and return current note ID when note equal to noteId (required params ID)
+        contents = contents.filter(currentNote => {
+           return currentNote.id != noteId;
+        });
+        // For current note data, change note ID to string and add to newId (originally 0)
+        for (currentNote of contents) {
+            currentNote.id = newId.toString();
+            newId++;
         }
+        // Write to files with updated json data
+        fs.writeFileSync("./db/db.json", JSON.stringify(contents));
         res.json(contents);
-
     });
 };
 
